1b91387cf0ffd0f88b81a374d161bba25fc870b5
LuisLara525 <luiss.lara@correo.usa.edu.co>
2020-06-10 21:50:05 -0500

    ideas modificado

--- Proyecto/src/main/java/com/proyectoabp/ws/mod/dao/DAOActivity.java
+++ Proyecto/src/main/java/com/proyectoabp/ws/mod/dao/DAOActivity.java
@@ -2,15 +2,20 @@ package com.proyectoabp.ws.mod.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import com.google.gson.Gson;
 import com.proyectoabp.ws.mod.conexion.Conexion;
 import com.proyectoabp.ws.rest.vo.VOActividad;
+import com.proyectoabp.ws.rest.vo.VOCalificacion;
 import com.proyectoabp.ws.rest.vo.VOHipotesis;
 import com.proyectoabp.ws.rest.vo.VOIdeas;
 import com.proyectoabp.ws.rest.vo.VOPalabras;
 import com.proyectoabp.ws.rest.vo.VOProblema;
 import com.proyectoabp.ws.rest.vo.VORespuesta;
+import com.proyectoabp.ws.rest.vo.VOSesiones;
 
 public class DAOActivity {
 // PROBLEMA ////////////////////////
@@ -39,7 +44,35 @@ public class DAOActivity {
 			return true;
 		}
 	}
-
+	
+	public String GetProblema(VOProblema vo) {
+		String h=null;
+		Gson gson= new Gson();
+		ArrayList<VOProblema> grupos= new ArrayList<VOProblema>();
+		PreparedStatement preparedStmt = null;
+		String query = "select * from problema where NumG=?";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			//preparedStmt.setString(1, vo.getProblem());
+			preparedStmt.setString(1, vo.getNum1());
+			ResultSet rs = preparedStmt.executeQuery();
+			String code=null;
+			String est= null;
+			String nombre = null;
+			String profe = null;
+			while (rs.next()) {
+				code=rs.getString("problem");
+				est= rs.getString("NumG");
+				grupos.add(new VOProblema(code, est));
+			}
+			h= gson.toJson(grupos);
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return h;
+	}
 	// IDEAS ///////////////////////
 	public boolean SubirIdea(VOIdeas vo)  throws SQLException{
 		int result = 0;
@@ -66,6 +99,35 @@ public class DAOActivity {
 		}
 	}
 	
+	public String GetIdea(VOIdeas vo) {
+		String h=null;
+		Gson gson= new Gson();
+		ArrayList<VOIdeas> grupos= new ArrayList<VOIdeas>();
+		PreparedStatement preparedStmt = null;
+		String query = "select * from ideas where NumG=?";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			//preparedStmt.setString(1, vo.getIdea());
+			preparedStmt.setString(1, vo.getNum2());
+			ResultSet rs = preparedStmt.executeQuery();
+			String code=null;
+			String est= null;
+			String nombre = null;
+			String profe = null;
+			while (rs.next()) {
+				code=rs.getString("idea");
+				est= rs.getString("NumG");
+				grupos.add(new VOIdeas(code, est));
+			}
+			h= gson.toJson(grupos);
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return h;
+	}
+	
 	// HIPOTESIS ////////////////////////////
 	public boolean SubirHipo(VOHipotesis vo)  throws SQLException{
 		int result = 0;
@@ -90,6 +152,35 @@ public class DAOActivity {
 		} else {
 			return true;
 		}
+	}
+	
+	public String GetHipotesis(VOHipotesis vo) {
+		String h=null;
+		Gson gson= new Gson();
+		ArrayList<VOHipotesis> grupos= new ArrayList<VOHipotesis>();
+		PreparedStatement preparedStmt = null;
+		String query = "select * from hipotesis where NumG=?";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			//preparedStmt.setString(1, vo.getHipo());
+			preparedStmt.setString(1, vo.getNum3());
+			ResultSet rs = preparedStmt.executeQuery();
+			String code=null;
+			String est= null;
+			String nombre = null;
+			String profe = null;
+			while (rs.next()) {
+				code=rs.getString("hipo");
+				est= rs.getString("NumG");
+				grupos.add(new VOHipotesis(code, est));
+			}
+			h= gson.toJson(grupos);
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return h;
 	}
 	
 	// PALABRAS CLAVE
@@ -119,6 +210,35 @@ public class DAOActivity {
 		}
 	}
 	
+	public String GetPalabras(VOPalabras vo) {
+		String h=null;
+		Gson gson= new Gson();
+		ArrayList<VOPalabras> grupos= new ArrayList<VOPalabras>();
+		PreparedStatement preparedStmt = null;
+		String query = "select * from palabras_clave where NumG=?";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			//preparedStmt.setString(1, vo.getPalabras());
+			preparedStmt.setString(1, vo.getNum4());
+			ResultSet rs = preparedStmt.executeQuery();
+			String code=null;
+			String est= null;
+			String nombre = null;
+			String profe = null;
+			while (rs.next()) {
+				code=rs.getString("palabras");
+				est= rs.getString("NumG");
+				grupos.add(new VOPalabras(code, est));
+			}
+			h= gson.toJson(grupos);
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return h;
+	}
+	
 	//IDEAS 2
 	
 	public boolean SubirIdea2(VOIdeas vo)  throws SQLException{
@@ -144,6 +264,34 @@ public class DAOActivity {
 		} else {
 			return true;
 		}
+	}
+	public String GetIdea2(VOIdeas vo) {
+		String h=null;
+		Gson gson= new Gson();
+		ArrayList<VOIdeas> grupos= new ArrayList<VOIdeas>();
+		PreparedStatement preparedStmt = null;
+		String query = "select * from ideas2 where NumG=?";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			//preparedStmt.setString(1, vo.getIdea());
+			preparedStmt.setString(1, vo.getNum2());
+			ResultSet rs = preparedStmt.executeQuery();
+			String code=null;
+			String est= null;
+			String nombre = null;
+			String profe = null;
+			while (rs.next()) {
+				code=rs.getString("idea");
+				est= rs.getString("NumG");
+				grupos.add(new VOIdeas(code, est));
+			}
+			h= gson.toJson(grupos);
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return h;
 	}
 	//RESPUESTA
 	
@@ -171,4 +319,140 @@ public class DAOActivity {
 			return true;
 		}
 	}
+	public String GetRespuesta(VORespuesta vo) {
+		String h=null;
+		Gson gson= new Gson();
+		ArrayList<VORespuesta> grupos= new ArrayList<VORespuesta>();
+		PreparedStatement preparedStmt = null;
+		String query = "select * from respuesta where NumG=?";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			//preparedStmt.setString(1, vo.getRespuesta());
+			preparedStmt.setString(1, vo.getNum6());
+			ResultSet rs = preparedStmt.executeQuery();
+			String code=null;
+			String est= null;
+			String nombre = null;
+			String profe = null;
+			while (rs.next()) {
+				code=rs.getString("hipo");
+				est= rs.getString("NumG");
+				grupos.add(new VORespuesta(code, est));
+			}
+			h= gson.toJson(grupos);
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return h;
+	}
+	// CALIFICACION
+	public boolean cali(VOCalificacion vo)  throws SQLException{
+		int result = 0;
+		PreparedStatement preparedStmt = null;
+		String query = "insert into calificaciones(calificacion, nombre) values(?,?);";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			preparedStmt.setString(1, vo.getCali());
+			preparedStmt.setString(2, vo.getNamS());
+			
+			result = preparedStmt.executeUpdate();
+			String usera = null;
+			String password = null;
+			String tipo = null;
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (result == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	public String GetCali(VOCalificacion vo) {
+		String h=null;
+		Gson gson= new Gson();
+		ArrayList<VOIdeas> grupos= new ArrayList<VOIdeas>();
+		PreparedStatement preparedStmt = null;
+		String query = "select * from calificaciones";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			//preparedStmt.setString(1, vo.getCali());
+			//preparedStmt.setString(2, vo.getNamS());
+			ResultSet rs = preparedStmt.executeQuery();
+			String code=null;
+			String est= null;
+			String nombre = null;
+			String profe = null;
+			while (rs.next()) {
+				code=rs.getString("calificacion");
+				est= rs.getString("nombre");
+				grupos.add(new VOIdeas(code, est));
+			}
+			h= gson.toJson(grupos);
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return h;
+	}
+	//CALIFICACION FINAL
+	public boolean cali2(VOCalificacion vo)  throws SQLException{
+		int result = 0;
+		PreparedStatement preparedStmt = null;
+		String query = "insert into calificacion_final(calificacion, nombre) values(?,?);";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			preparedStmt.setString(1, vo.getCali());
+			preparedStmt.setString(2, vo.getNamS());
+			
+			result = preparedStmt.executeUpdate();
+			String usera = null;
+			String password = null;
+			String tipo = null;
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (result == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public String GetCali2(VOCalificacion vo) {
+		String h=null;
+		Gson gson= new Gson();
+		ArrayList<VOIdeas> grupos= new ArrayList<VOIdeas>();
+		PreparedStatement preparedStmt = null;
+		String query = "select * from calificacion_final";
+		try {
+			Connection connection = Conexion.getConenction();
+			preparedStmt = connection.prepareStatement(query);
+			//preparedStmt.setString(1, vo.getCali());
+			//preparedStmt.setString(2, vo.getNamS());
+			ResultSet rs = preparedStmt.executeQuery();
+			String code=null;
+			String est= null;
+			String nombre = null;
+			String profe = null;
+			while (rs.next()) {
+				code=rs.getString("calificacion");
+				est= rs.getString("nombre");
+				grupos.add(new VOIdeas(code, est));
+			}
+			h= gson.toJson(grupos);
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return h;
+	}
+
 }
--- Proyecto/src/main/java/com/proyectoabp/ws/rest/service/ServicesActivity.java
+++ Proyecto/src/main/java/com/proyectoabp/ws/rest/service/ServicesActivity.java
@@ -11,12 +11,15 @@ import javax.ws.rs.core.Response;
 
 import com.proyectoabp.ws.mod.dao.DAOActividad;
 import com.proyectoabp.ws.mod.dao.DAOActivity;
+import com.proyectoabp.ws.mod.dao.DAOSesiones;
 import com.proyectoabp.ws.rest.vo.VOActividad;
+import com.proyectoabp.ws.rest.vo.VOCalificacion;
 import com.proyectoabp.ws.rest.vo.VOHipotesis;
 import com.proyectoabp.ws.rest.vo.VOIdeas;
 import com.proyectoabp.ws.rest.vo.VOPalabras;
 import com.proyectoabp.ws.rest.vo.VOProblema;
 import com.proyectoabp.ws.rest.vo.VORespuesta;
+import com.proyectoabp.ws.rest.vo.VOSesiones;
 
 @Path("/Activity")
 public class ServicesActivity {
@@ -40,6 +43,23 @@ public class ServicesActivity {
 		}
         return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
     }
+	@POST
+    @Path("/ProblemaGet")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response sesion(VOProblema vo) {
+		//System.out.println(vo.getProfesorsito());
+        DAOActivity dao = new DAOActivity();        
+        try {        	
+				String aux = (String) dao.GetProblema(vo);
+				return Response.status(Response.Status.CREATED).entity(aux).build();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
+	
 	// IDEAS
 	
 	@POST
@@ -60,6 +80,23 @@ public class ServicesActivity {
 		}
         return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
     }
+	@POST
+    @Path("/IdeaGet")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response sesion1(VOIdeas vo) {
+		//System.out.println(vo.getProfesorsito());
+        DAOActivity dao = new DAOActivity();        
+        try {        	
+				String aux = (String) dao.GetIdea(vo);
+				return Response.status(Response.Status.CREATED).entity(aux).build();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
+	
 	// HIPOTESIS
 	@POST
     @Path("/HipotesisSubir")
@@ -79,7 +116,22 @@ public class ServicesActivity {
 		}
         return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
 	}
-	
+	@POST
+    @Path("/HipotesisGet")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response sesion2(VOHipotesis vo) {
+		//System.out.println(vo.getProfesorsito());
+        DAOActivity dao = new DAOActivity();        
+        try {        	
+				String aux = (String) dao.GetHipotesis(vo);
+				return Response.status(Response.Status.CREATED).entity(aux).build();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
 	
 	// PALABRAS CLAVE
 	@POST
@@ -100,6 +152,22 @@ public class ServicesActivity {
 		}
         return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
 	}
+	@POST
+    @Path("/PalabrasGet")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response sesion3(VOPalabras vo) {
+		//System.out.println(vo.getProfesorsito());
+        DAOActivity dao = new DAOActivity();        
+        try {        	
+				String aux = (String) dao.GetPalabras(vo);
+				return Response.status(Response.Status.CREATED).entity(aux).build();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
 	//IDEAS 2
 	
 	@POST
@@ -120,12 +188,28 @@ public class ServicesActivity {
 		}
         return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
     }
+	@POST
+    @Path("/IdeaGet2")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response sesion5(VOIdeas vo) {
+		//System.out.println(vo.getProfesorsito());
+        DAOActivity dao = new DAOActivity();        
+        try {        	
+				String aux = (String) dao.GetIdea2(vo);
+				return Response.status(Response.Status.CREATED).entity(aux).build();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
 	//RESPUESTA
 	@POST
     @Path("/RespuestaSubir")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public Response a5(VORespuesta vo) {
+    public Response a6(VORespuesta vo) {
         DAOActivity dao = new DAOActivity();        
         try {
 			if(dao.Respuesta(vo)!=false) {
@@ -135,6 +219,93 @@ public class ServicesActivity {
 				return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
 			}
 		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
+	@POST
+    @Path("/RespuestaGet")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response sesion6(VORespuesta vo) {
+		//System.out.println(vo.getProfesorsito());
+        DAOActivity dao = new DAOActivity();        
+        try {        	
+				String aux = (String) dao.GetRespuesta(vo);
+				return Response.status(Response.Status.CREATED).entity(aux).build();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
+	//CALIFICACION
+	@POST
+    @Path("/CalificacionSubir")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response a7(VOCalificacion vo) {
+        DAOActivity dao = new DAOActivity();        
+        try {
+			if(dao.cali(vo)!=false) {
+					return Response.status(Response.Status.CREATED).entity("{\"Status\": \"hecho\"}").build();
+				
+			}else {
+				return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
+	@POST
+    @Path("/CalificacionGet")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response sesion7(VOCalificacion vo) {
+		//System.out.println(vo.getProfesorsito());
+        DAOActivity dao = new DAOActivity();        
+        try {        	
+				String aux = (String) dao.GetCali(vo);
+				return Response.status(Response.Status.CREATED).entity(aux).build();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
+	
+	// CALIFICACION FINAL
+	@POST
+    @Path("/CalificacionSubir2")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response a8(VOCalificacion vo) {
+        DAOActivity dao = new DAOActivity();        
+        try {
+			if(dao.cali2(vo)!=false) {
+					return Response.status(Response.Status.CREATED).entity("{\"Status\": \"hecho\"}").build();
+				
+			}else {
+				return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
+    }
+	@POST
+    @Path("/CalificacionGet2")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response sesion8(VOCalificacion vo) {
+		//System.out.println(vo.getProfesorsito());
+        DAOActivity dao = new DAOActivity();        
+        try {        	
+				String aux = (String) dao.GetCali2(vo);
+				return Response.status(Response.Status.CREATED).entity(aux).build();
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
         return Response.status(Response.Status.CREATED).entity("{\"Status\": \"Error\"}").build();
